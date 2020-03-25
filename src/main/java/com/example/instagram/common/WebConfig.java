6a0c27a097e4e91fd72a098039ddf5d031ffad0d
kimjaeyun <je941004@naver.com>
2020-03-26 00:45:49 +0900

    cors 설정

--- src/main/java/com/example/instagram/common/WebConfig.java
+++ src/main/java/com/example/instagram/common/WebConfig.java
@@ -2,6 +2,7 @@ package com.example.instagram.common;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,4 +18,11 @@ public class WebConfig implements WebMvcConfigurer {
     registry.addInterceptor(jwtInterceptor).addPathPatterns("/api/**")
         .excludePathPatterns(EXCLUDE_PATHS);
   }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("*");
+  }
+
 }
