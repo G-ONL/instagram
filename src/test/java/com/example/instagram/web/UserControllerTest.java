9a01e2f217405ec273697a49789b362e6d8e85a1
kimjaeyun <je941004@naver.com>
2020-04-01 20:34:50 +0900

    테스트 코드 수정

--- src/test/java/com/example/instagram/web/UserControllerTest.java
+++ src/test/java/com/example/instagram/web/UserControllerTest.java
@@ -3,10 +3,9 @@ package com.example.instagram.web;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.instagram.domain.user.UserRepository;
+import com.example.instagram.service.UserService;
 import com.example.instagram.web.dto.ResponseDataDto;
 import com.example.instagram.web.dto.user.UserJoinRequestDto;
-import com.example.instagram.web.dto.user.UserLoginResponseDto;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.After;
 import org.junit.Test;
@@ -35,22 +34,26 @@ public class UserControllerTest {
   @Autowired
   private TestRestTemplate testRestTemplate;
 
+  @Autowired
+  private UserService userService;
+
   @After
-  public void tearDown() throws Exception {
+  public void tearDown() {
     userRepository.deleteAll();
   }
 
   @Test
-  public void 유저가_회원가입_성공() throws Exception {
+  public void 유저가_회원가입_성공() {
     //given
     String userName = "abc@naver.com";
     String password = "12345";
-    String url = "http://localhost:" + port + "/user/join";
+    String url = "/user/join";
     UserJoinRequestDto userJoinRequestDto = new UserJoinRequestDto(userName, password);
 
     //when
     ResponseEntity responseEntity = testRestTemplate
         .postForEntity(url, userJoinRequestDto, ResponseDataDto.class);
+
     //then
     assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
     ResponseDataDto responseDataDto = (ResponseDataDto) responseEntity.getBody();
@@ -58,11 +61,27 @@ public class UserControllerTest {
     assertThat(responseDataDto.getStatus()).isEqualTo(200);
     assertThat(responseDataDto.getMessage()).isEqualTo("Success");
 
-    UserLoginResponseDto userLoginResponseDto = mapper
-        .convertValue(responseDataDto.getData().get(0), new TypeReference<>() {
-        });
+  }
 
-    assertThat(userLoginResponseDto.getUserId()).isEqualTo(1L);
+  @Test
+  public void 중복_회원_가입() {
+    //given
+    String userName = "abc@naver.com";
+    String password = "12345";
+    String url = "/user/join";
+    UserJoinRequestDto userJoinRequestDto = new UserJoinRequestDto(userName, password);
+    UserJoinRequestDto duplicateUser = new UserJoinRequestDto(userName, password);
+    userService.join(userJoinRequestDto);
+    //when
+    ResponseEntity responseEntity = testRestTemplate
+        .postForEntity(url, userJoinRequestDto, ResponseDataDto.class);
+    //then
+    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    ResponseDataDto responseDataDto = (ResponseDataDto) responseEntity.getBody();
+    assertThat(responseDataDto).isInstanceOf(ResponseDataDto.class);
+    assertThat(responseDataDto.getStatus()).isEqualTo(400);
+    assertThat(responseDataDto.getMessage()).isEqualTo("이미 등록된 아이디 입니다.");
+
   }
 
 }
