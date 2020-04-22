5d4fa93d4f716f965f5f9bcbfc7bae415e3db880
kimjaeyun <je941004@naver.com>
2020-04-22 22:11:20 +0900

    토큰 설정

--- src/main/java/com/example/instagram/common/WebConfig.java
+++ src/main/java/com/example/instagram/common/WebConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
   private final JwtInterceptor jwtInterceptor;
-  private static final String[] EXCLUDE_PATHS = {"/resource/**", "/static/**", "/user/**",
+  private static final String[] EXCLUDE_PATHS = {"/resource/**", "/static/**", "/user/login/**", "/user/join/**",
       "/error/**"};
 
   @Override
