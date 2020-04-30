2245fbb3a43c6a6af7ac128cff0f4bf2ae3938ac
kimjaeyun <je941004@naver.com>
2020-05-01 02:02:03 +0900

    comments 수정

--- src/main/java/com/example/instagram/web/dto/comment/CommentResponseDto.java
+++ src/main/java/com/example/instagram/web/dto/comment/CommentResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.instagram.web.dto.comment;
 
 import com.example.instagram.domain.comment.Comment;
+import com.example.instagram.domain.user.User;
 import com.example.instagram.web.dto.post.PostResponseDto;
 import lombok.Getter;
 
@@ -9,9 +10,15 @@ public class CommentResponseDto {
 
   private Long id;
   private String comment;
+  private Long userId;
+  private String userName;
 
   public CommentResponseDto(Comment comment) {
+    User user = comment.getUser();
     this.id = comment.getId();
     this.comment = comment.getComment();
+    this.userName = user.getUserName();
+    this.userId = user.getId();
+
   }
 }
